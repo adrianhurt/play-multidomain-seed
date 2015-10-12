96c3c53e3eef655cfb8edbd3ea4a05b2d036859e
Adrian Hurtado <adrianhurt@gmail.com>
2015-10-12 21:19:38 +0200

    Added custom ErrorHandler to AssetsBuilder

--- modules/common/app/controllers/Assets.scala
+++ modules/common/app/controllers/Assets.scala
@@ -1,13 +1,13 @@
 package controllers.common
 
 import controllers.AssetsBuilder
-import play.api.http.DefaultHttpErrorHandler
+import play.api.http.HttpErrorHandler
 import controllers.Assets.Asset
 import play.api.mvc.{Controller, Action}
 import java.io.File
 
 
-class Assets extends AssetsBuilder(DefaultHttpErrorHandler) {
+class Assets(errorHandler: HttpErrorHandler) extends AssetsBuilder(errorHandler) {
 	def public (path: String, file: Asset) = versioned(path, file)
 	def lib (path: String, file: Asset) = versioned(path, file)
 	def css (path: String, file: Asset) = versioned(path, file)
--- modules/web/app/controllers/Assets.scala
+++ modules/web/app/controllers/Assets.scala
@@ -1,4 +1,4 @@
 package controllers.web
 
-class Assets extends controllers.common.Assets
+class Assets @javax.inject.Inject() (val errorHandler: web.ErrorHandler) extends controllers.common.Assets(errorHandler)
 class SharedResources extends controllers.common.SharedResources
