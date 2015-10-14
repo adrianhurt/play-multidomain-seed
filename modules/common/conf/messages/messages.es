3f2b36e0885e8adad931eae8be149d14d3b9b80a
Adrian Hurtado <adrianhurt@gmail.com>
2015-10-14 17:41:02 +0200

    Added localization for errors

--- modules/common/conf/messages/messages.es
+++ modules/common/conf/messages/messages.es
@@ -15,3 +15,9 @@ computer.company=Compañía
 modal.title=Título del modal
 modal.text=Esto es un modal
 modal.close=Cerrar
+
+
+error.unknown.title=Oops, ha ocurrido un error
+error.unknown.text=Esta excepción ha sido registrada con el id <strong>{0}</strong>.
+error.notfound.title=Dirección no encontrada
+error.notfound.text=Para la petición ''{0}''
--- modules/web/app/ErrorHandler.scala
+++ modules/web/app/ErrorHandler.scala
@@ -4,6 +4,7 @@ import play.api.http.DefaultHttpErrorHandler
 import play.api._
 import play.api.mvc._
 import play.api.mvc.Results._
+import play.api.i18n.{ I18nSupport, MessagesApi, Messages }
 import play.api.routing.Router
 import scala.concurrent._
 import javax.inject._
@@ -12,18 +13,19 @@ class ErrorHandler @Inject() (
     env: Environment,
     config: Configuration,
     sourceMapper: OptionalSourceMapper,
-    router: Provider[Router]) extends DefaultHttpErrorHandler(env, config, sourceMapper, router) {
+    router: Provider[Router],
+    val messagesApi: MessagesApi) extends DefaultHttpErrorHandler(env, config, sourceMapper, router) with I18nSupport {
 
   // 404 - page not found error
   override def onNotFound(request: RequestHeader, message: String): Future[Result] = Future.successful {
     NotFound(env.mode match {
-      case Mode.Prod => views.html.web.errors.notFound(request)
+      case Mode.Prod => views.html.web.errors.notFound(request)(request2Messages(request))
       case _ => views.html.defaultpages.devNotFound(request.method, request.uri, Some(router.get))
     })
   }
 
   // 500 - internal server error
   override def onProdServerError(request: RequestHeader, exception: UsefulException) = Future.successful {
-    InternalServerError(views.html.web.errors.error(request, exception))
+    InternalServerError(views.html.web.errors.error(request, exception)(request2Messages(request)))
   }
 }
